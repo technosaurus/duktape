32077cbafdcccf848d67bd94c97566bcf6940730
Sami Vaarala <sami.vaarala@iki.fi>
2014-05-15 00:21:48 +0300

    more icon sizes

--- website/resizeicons.sh
+++ website/resizeicons.sh
@@ -3,7 +3,7 @@
 #  Resize touch icon starting from touch_icon_1024x1024.png.
 #
 
-for sz in 57 60 72 114 120 144 152; do
+for sz in 16 32 48 57 60 64 72 114 120 128 144 152 256; do
 	convert touch_icon_1024x1024.png -resize ${sz}x${sz} touch_icon_${sz}x${sz}.png
 	file touch_icon_${sz}x${sz}.png
 done
